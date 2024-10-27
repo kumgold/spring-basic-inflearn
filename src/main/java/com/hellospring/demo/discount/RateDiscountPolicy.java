fec2693a4bf167710eceff244bfda9bfaaf13cf7
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-27 11:23:42 +0900

    새로운 할인 정책 개발

---
+++ src/main/java/com/hellospring/demo/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.hellospring.demo.discount;
+
+import com.hellospring.demo.member.Grade;
+import com.hellospring.demo.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private final int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
